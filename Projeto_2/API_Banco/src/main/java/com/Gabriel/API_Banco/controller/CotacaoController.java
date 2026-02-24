6be934729f08a0b93c54cab8b445e84f86d1a7c9
Gabriel <gabrieljoschem@gmail.com>
2026-02-24 17:03:10 -0300

    Criação do controller de cotações, faltando criar o service e a DTO

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/controller/CotacaoController.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/controller/CotacaoController.java
@@ -1,9 +1,13 @@
 package com.Gabriel.API_Banco.controller;
 
 import com.Gabriel.API_Banco.dto.CriarCotacaoDTO;
+import com.Gabriel.API_Banco.dto.ListarLentesDTO;
 import com.Gabriel.API_Banco.model.Cotacao;
 import com.Gabriel.API_Banco.service.CotacaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*") // Permite requisições do frontend
 @RestController
@@ -21,5 +25,8 @@ public class CotacaoController {
         return cotacaoService.criarCotacao(dto);
     }
 
-    @GetMapping("/listarCotacoes")
+    @GetMapping("/listarCotacoesPU/{idUsuario}")
+    public ResponseEntity<List<ListarCotacoesDTO>> ListarPorUsuario(@PathVariable Long idUsuario) {
+        return ResponseEntity.ok(cotacaoService.listarPorUsuario(idUsuario));
+    }
 }
