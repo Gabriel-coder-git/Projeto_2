0b9463c79ae8523eba34af9aa3a0a84aad02c613
Gabriel <gabrieljoschem@gmail.com>
2026-02-24 22:28:51 -0300

    criação do método de find by id do usuário

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/CotacaoRepositorio.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/CotacaoRepositorio.java
@@ -3,7 +3,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Gabriel.API_Banco.model.Cotacao;
 
+import java.util.Optional;
+
 public interface CotacaoRepositorio extends JpaRepository<Cotacao, Long>{
     
-
+    Optional<Cotacao> findByUsuarioId(Long idUsuario);
 }
