428eb163b8eb311a7e18d1ebd6f7cae7fdedc51d
Gabriel <gabrieljoschem@gmail.com>
2026-02-24 22:29:35 -0300

    DTO de listagem da cotação

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/ListarCotacoesDTO.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/ListarCotacoesDTO.java
@@ -1,6 +1,15 @@
 package com.Gabriel.API_Banco.dto;
 
-public class ListarCotacoesDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListarCotacoesDTO {
+    private Long idUsuario;
+    private Long idLoja;
+    private ListarProdutosDTO produto;
+
 }
