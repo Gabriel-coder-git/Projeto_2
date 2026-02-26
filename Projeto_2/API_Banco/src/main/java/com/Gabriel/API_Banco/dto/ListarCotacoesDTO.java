9471d8d0e730bb3d7d2fdd6101102a399a1ccdfb
Gabriel <gabrieljoschem@gmail.com>
2026-02-26 02:13:10 -0300

    Adição de alguns dados que estavam faltando na dto de listagem da cotação

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/ListarCotacoesDTO.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/ListarCotacoesDTO.java
@@ -1,15 +1,28 @@
 package com.Gabriel.API_Banco.dto;
 
+import com.Gabriel.API_Banco.model.Loja;
+import com.Gabriel.API_Banco.model.enums.StatusCotacao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ListarCotacoesDTO {
     private Long idUsuario;
-    private Long idLoja;
+    private Loja loja;
     private ListarProdutosDTO produto;
-
+    private BigDecimal valorBase;
+    private BigDecimal valorFinal;
+    private Integer prazoEntrega;
+    private LocalDate dataCriacao;
+    private LocalDate dataResposta;
+    private LocalDate dataAprovacao;
+    private String obsCliente;
+    private String obsLoja;
+    private StatusCotacao status;
 }
