a2da205a5a6af2c60d7dcc82ee83d006962cf591
Gabriel <gabrieljoschem@gmail.com>
2026-02-27 09:28:12 -0300

    Lógica de get e set da foto de perfil do usuário

--- Projeto_2/JS/core/imgs.js
+++ Projeto_2/JS/core/imgs.js
@@ -1,61 +1,69 @@
-import {API} from "../core/api.js";
-import {getUsuarioLogado} from "../core/auth.js";
+import { API } from "../core/api.js";
+import { getUsuarioLogado } from "../core/auth.js";
 
 const form = document.getElementById("form-foto-perfil");
 const inputFoto = document.getElementById("inputFoto");
 const imgFoto = document.getElementById("fotoPerfil");
 
-const usuarioId = getUsuarioLogado().id;
+export function salvarFotoPerfil() {
 
+    if (!form) return; // evita erro se não existir na página
 
-export async function salvarFotoPerfil() {
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+        const usuario = getUsuarioLogado();
+        const usuarioId = usuario.id;
 
-    const file = inputFoto.files[0];
+        const file = inputFoto.files[0];
 
-    if (!file) {
-        alert("Selecione uma imagem!");
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", file);
-
-    try {
-        const response = await fetch(
-            `${API}/usuarios/setarFotoPerfil/${usuarioId}`,
-            {
-                method: "POST",
-                body: formData
-            }
-        );
-
-        if (!response.ok) {
-            throw new Error("Erro ao enviar imagem");
+        if (!file) {
+            alert("Selecione uma imagem!");
+            return;
         }
 
-        const imageUrl = await response.text();
+        const formData = new FormData();
+        formData.append("file", file);
 
-        // Atualiza a imagem na tela
-        imgFoto.src = imageUrl;
+        try {
+            const response = await fetch(
+                `${API}/usuarios/setarFotoPerfil/${usuarioId}`,
+                {
+                    method: "POST",
+                    body: formData
+                }
+            );
 
-        alert("Foto atualizada com sucesso!");
+            if (!response.ok) {
+                throw new Error("Erro ao enviar imagem");
+            }
 
-    } catch (error) {
-        console.error(error);
-        alert("Erro ao atualizar foto");
-    }
-})}
+            const imageUrl = await response.text();
+
+            // Atualiza imagem na tela
+            imgFoto.src = imageUrl;
+
+            //Atualiza localStorage
+            usuario.fotoUrl = imageUrl;
+            localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+
+            alert("Foto atualizada com sucesso!");
+
+        } catch (error) {
+            console.error(error);
+            alert("Erro ao atualizar foto");
+        }
+    });
+}
 
 
-export async function carregarFotoUsuario() {
-    const response = await fetch(`${API}/usuarios/getFotoPerfil/${usuarioId}`);
-    const usuario = await response.json();
+export function carregarFotoUsuario(usuario) {
+    console.log("FotoUrl recebida:", usuario.fotoUrl);
+    if (!imgFoto) return;
 
-    if (usuario.fotoUrl && usuario.fotoUrl.trim() !== "") {
+    if (usuario?.fotoUrl && usuario.fotoUrl.trim() !== "") {
         imgFoto.src = usuario.fotoUrl;
+        console.log("FotoUrl recebida:", usuario.fotoUrl);
     } else {
         imgFoto.src = "imgs/9742847.png";
     }
